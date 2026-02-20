f969657c4a941120f6bab12219a68fbe0683beac
Ricardo Morataya <ricardojms1999@gmail.com>
2026-02-20 15:37:19 -0600

    Remove wrapper markup from Tetris page to simplify component hierarchy

--- app/tetris/page.tsx
+++ app/tetris/page.tsx
@@ -1,10 +1,5 @@
 import TetrisClient from "@/features/tetris/TetrisClient";
 
 export default function TetrisPage() {
-  return (
-    <main style={{ padding: 16 }}>
-      <h1 style={{ marginBottom: 12 }}>Tetris</h1>
-      <TetrisClient />
-    </main>
-  );
+  return <TetrisClient />;
 }
